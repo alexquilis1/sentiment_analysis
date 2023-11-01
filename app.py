4bdc68fbbd6641c35d4cdd2b843b27b73c7626fd
Alex Quilis Vila <alexquilis1@gmail.com>
2023-11-01 17:06:00 +0100

    updated whole project

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, send_from_directory
+# Import neccessary libraries and modules
+from flask import Flask, render_template, request
 from newsapi.newsapi_client import NewsApiClient
 from config import API_KEY
 
@@ -9,46 +10,47 @@ import matplotlib.pyplot as plt
 import re
 import string
 import base64
-from PIL import Image
 from io import BytesIO
 
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 from nltk import word_tokenize, FreqDist
 
+# Initialize a Flask web application
 app = Flask(__name__)
 
+# Define the route for the main page
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html')    # Renders the 'index.html' template
 
-@app.route('/consultar_noticias', methods=['POST'])
-def consultar_noticias():
-    user_topic = request.form['user_topic']
+# Define the route for processing user input and displaying results
+@app.route('/query_articles', methods=['POST'])
+def query_articles():
+    user_topic = request.form['user_topic'] # Get the user's topic of interest from the form
 
-    # Crea una instancia de la API de News
+    # Create an instance of the News API client
     newsapi = NewsApiClient(api_key=API_KEY)
 
-    # Llamar a la API de News y procesar los datos
+    # Call the News API to fecth news articles based on the user's topic
     all_articles = newsapi.get_everything(q=user_topic, language='en', sort_by='relevancy')
     df = pd.DataFrame(all_articles['articles'])
 
-    # Realizar el análisis de sentimiento
-    analyzer = SentimentIntensityAnalyzer()
+    # Perform sentiment ananlysis on the content of the articles
     df['content'] = df['content'].apply(preprocess_text)
     df['content_sentiment'] = df['content'].apply(get_sentiment)
 
-    # Filtrar noticias por sentimiento
+    # Filter news articles by sentiment
     positive_df = df[df['content_sentiment'] == 'positive']
     negative_df = df[df['content_sentiment'] == 'negative']
     neutral_df = df[df['content_sentiment'] == 'neutral']
 
-    # Crear gráficos y visualizaciones
+    # Create word clouds for each sentiment category
     wordcloud_positive = create_wordcloud(positive_df, 'Positive Sentiment')
     wordcloud_negative = create_wordcloud(negative_df, 'Negative Sentiment')
     wordcloud_neutral = create_wordcloud(neutral_df, 'Neutral Sentiment')
 
-    # Crear un gráfico de barras para mostrar la distribución de sentimientos
+    # Calculate the distribution of sentiments in the data
     sentiment_counts = df['content_sentiment'].value_counts()
     sentiments = sentiment_counts.index
     count = sentiment_counts.values
@@ -57,52 +59,36 @@ def consultar_noticias():
                            wordcloud_positive=wordcloud_positive, wordcloud_negative=wordcloud_negative, 
                            wordcloud_neutral=wordcloud_neutral)
 
-# Manejo de errores 404 (Recurso no encontrado)
-@app.errorhandler(404)
-def page_not_found(error):
-    return render_template('404.html'), 404
-
-# Manejo de errores 500 (Error interno del servidor)
-@app.errorhandler(500)
-def internal_server_error(error):
-    return render_template('500.html'), 500
-
-# Manejo de otros errores personalizados
-@app.errorhandler(Exception)
-def handle_exception(error):
-    # Loguea el error o realiza otras acciones necesarios
-    app.logger.error(f'Error inesperado: {error}')
-
-    # Devuelve una página de error personalizada al usuario
-    return render_template('error.html', error_message='Ocurrió un error inesperado.'), 500
-
+# Preprocess text data for sentiment analysis
 def preprocess_text(text):
-    # Convertir a minúsculas
+    # Convert to lowercase
     text = text.lower()
-    # Eliminar código HTML
+    # Remove HTML tags
     pattern = r'<[^>]+>'
     text = re.sub(pattern, '', text)
-    # Eliminar URL
+    # Remove URLs
     pattern = r'(http|ftp|https)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?'
     text = re.sub(pattern, '', text)
-    # Eliminar puntuación y caracteres especiales
+    # Remove punctuation and special characters
     text = ''.join([char for char in text if char not in string.punctuation])
-    # Eliminar la palabra "chars"
+    # Remove the word 'chars'
     text = text.replace('chars', '')
-    # Eliminar texto "via Getty Images"
+    # Remove text like 'via Getty Images'
     text = re.sub(r'\b(via|getty|images)\b', '', text)
-    # Tokenización
+    # Tokenize the text
     tokens = word_tokenize(text)
     fdist = FreqDist(tokens)
+    # Filter out low-frequency words
     filtered_words = [token for token in tokens if fdist[token] < fdist.N() * 0.1]
-    # Eliminar palabras vacías
+    # Remove stopwords
     stop_words = set(stopwords.words('english'))
     words = [word for word in filtered_words if word not in stop_words]
-    # Lematización
+    # Lemmatize words
     lemmatizer = WordNetLemmatizer()
     words = [lemmatizer.lemmatize(word) for word in words]
     return ' '.join(words)
 
+# Perform sentiment anañysis using VADER SentimentIntensityAnalyzer
 def get_sentiment(text):
     analyzer = SentimentIntensityAnalyzer()
     sentiment = analyzer.polarity_scores(text)
@@ -114,21 +100,23 @@ def get_sentiment(text):
     else:
         return 'neutral'
 
+# Generate a word cloud and return it as a base64-encoded image
 def create_wordcloud(dataframe, title):
     all_text = ' '.join(dataframe['content'])
     wordcloud = WordCloud(width=800, height=400, background_color='white').generate(all_text)
     
-    # Genera la imagen como un objeto BytesIO
+    # Generate the image as a BytesIO object
     img_buffer = BytesIO()
     wordcloud.to_image().save(img_buffer, format="PNG")
     
-    # Convierte el búfer de bytes en bytes
+    # Convert the bytes buffer to bytes
     img_bytes = img_buffer.getvalue()
 
-    # Convierte la imagen en base64
+    # Convert the image to base64 format
     image_base64 = base64.b64encode(img_bytes).decode('utf8')
     
     return image_base64
-    
+
+# Start the Flask application when executed directly
 if __name__ == '__main__':
     app.run(debug=True)
